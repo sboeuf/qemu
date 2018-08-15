a7a8577e41977b379c9dc4ea1db486d777c7a25f
Miklos Szeredi <mszeredi@redhat.com>
2018-08-15 17:05:29 +0200

    virtiofsd: passthrough_ll: add renameat2 support
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- tools/virtiofsd/passthrough_ll.c
+++ tools/virtiofsd/passthrough_ll.c
@@ -1100,7 +1100,17 @@ static void lo_rename(fuse_req_t req, fuse_ino_t parent, const char *name,
     }
 
     if (flags) {
+#ifndef SYS_renameat2
         fuse_reply_err(req, EINVAL);
+#else
+        res = renameat2(lo_fd(req, parent), name,
+                       lo_fd(req, newparent), newname, flags);
+        if (res == -1 && errno == ENOSYS) {
+            fuse_reply_err(req, EINVAL);
+        } else {
+            fuse_reply_err(req, res == -1 ? errno : 0);
+        }
+#endif
         return;
     }
 
